c9c55de26a904bdd28236f888615c38b1fb4e2c6
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-10 05:25:55 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+let name = "Earl";
+let age = 40;
+let rank = "Captain";
+
+if (age < 18) {
+    console.log("You are too young for the military!")
+} else if (name === "Rugen" && rank === "Count") {
+    console.log("My name is Inigo Montoya, you killed my father, prepare to die.")
+} else if (rank === "Captain") {
+    console.log(`Aye Aye Captain ${name}`)
+} else if (rank === "Private" && age > 40) {
+    console.log(`What have you been doing with your life, ${name}?`)
+} else {
+    console.log(`Hello ${rank} ${name}, I cannot wait to celebrate your ${age + 1}th birthday next year!`)
+}
